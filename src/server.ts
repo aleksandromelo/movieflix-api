d0827c47746fbc103accd07e6021008280a5cca3
aleksandromelo <adantasm@gmail.com>
2023-06-22 06:59:56 -0300

    configurações do express

--- src/server.ts
+++ src/server.ts
@@ -15,3 +15,17 @@
 // server.listen(3000, () => {
 //  console.log(`Servidor em execução em http://localhost:3000/`);
 // });
+
+import { log } from "console";
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/movies", (req, res) => {
+    res.send("Listagem de filmes");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor em execução na porta ${port}`);
+});
